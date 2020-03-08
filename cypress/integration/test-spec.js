afeac3d164010b6f57b4e1bf7615d4b516cae027
guisalmeida <guisalmeida.dev@gmail.com>
2020-03-08 20:13:25 -0300

    add cypress test

---
+++ cypress/integration/test-spec.js
@@ -0,0 +1,33 @@
+describe('Test form', function(){
+    it ('Test link', function(){
+        cy.visit('/');
+
+        cy.url()
+            .should('include', ':8080');
+
+        cy.pause();
+    });
+    
+    it ('Test title', function(){
+        cy.get('h1')
+        .should('contain', 'Comentários');
+        
+        cy.pause();
+    });
+    
+    it('Navegation test', function(){
+        cy.get('input.form-control')
+            .type('Usuario');
+        
+        cy.get('textarea.form-control')
+            .type('Mensagem de teste.');
+        
+        cy.get('button.btn')
+            .click();
+        
+        cy.get('a.delete')
+            .click();
+        
+        cy.pause();
+    });
+});
